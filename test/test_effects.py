4fe5211f85356f9ff83d16b0b79ff935ab202a40
Daniel Moisset <dmoisset@machinalis.com>
2008-02-28 03:18:28 +0000

    Added demo for effects

--- test/test_effects.py
+++ test/test_effects.py
@@ -10,9 +10,9 @@ sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
 from cocos.director import director
 from cocos.layer import Layer
 from cocos.scene import Scene
-from cocos.effect import TextureFilterEffect
+from cocos.effect import TextureFilterEffect, ColorizeEffect
 import pyglet
-
+from pyglet.gl import *
 
 class PictureLayer(Layer):
 
@@ -38,9 +38,14 @@ class MyEffect (TextureFilterEffect):
 
 if __name__ == "__main__":
     director.init()
+    # One little ball is scaled
     l1 = PictureLayer(50)
     l1.set_effect (MyEffect(0.5))
+    # The one above it is translucent
     l2 = PictureLayer(100)
-    director.run( Scene (l1, l2) )
+    l2.set_effect (ColorizeEffect(color=(1,1,1,0.75))) # 75 % opacity
+    # The onw above has no effect
+    l3 = PictureLayer(150)
+    director.run( Scene (l1, l2, l3) )
 
 
