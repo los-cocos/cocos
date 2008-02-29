61b479222f543f2e4dc1de75cee41bb655ee290d
Daniel Moisset <dmoisset@machinalis.com>
2008-02-29 23:53:21 +0000

    More description of each step

--- samples/hello_world.py
+++ samples/hello_world.py
@@ -2,7 +2,7 @@
 import sys
 import os
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
-
+#
 
 import cocos
 from cocos.director import director
@@ -11,12 +11,23 @@ from pyglet import font
 
 class HelloWorld(cocos.layer.Layer):
     def __init__(self):
+        # see pyglet documentation for help on this lines
         ft = font.load('Arial', 36)
         self.text = font.Text(ft, 'Hello, World!', x=100, y=240)
         
     def step(self, dt):
+        # this funcition is called on every frame
+        # dt is the elapsed time betwen this frame and the last        
         self.text.draw()
 
 if __name__ == "__main__":
+    # director init takes the same arguments as pyglet.window
     director.init()
-    director.run( cocos.scene.Scene( HelloWorld() ) )
+    # We create a new layer, an instance of HelloWorld
+    hello_layer = HelloWorld ()
+    # A scene that contains the layer hello_layer
+    main_scene = cocos.scene.Scene (hello_layer)
+    # And now, start the application, starting with main_scene
+    director.run (main_scene)
+    # or you could have written, without so many comments:
+    #      director.run( cocos.scene.Scene( HelloWorld() ) )
