db7f9537b8528c638506fa85ee0f148aca4b66e4
Ricardo Quesada <ricardoquesada@gmail.com>
2008-03-07 17:49:45 +0000

    tool to edir bezier curves

---
+++ tools/path_draw.py
@@ -0,0 +1,290 @@
+#
+# framework libs
+#
+
+import sys
+import os
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
+
+from pyglet.window import mouse, key
+from pyglet.gl import *
+from pyglet import font
+
+from cocos.director import director
+from cocos.layer import Layer
+from cocos.scene import Scene
+from cocos.path import Path, Bezier
+
+from primitives import Circle, Line
+import threading, string
+import time, cmd, math,random
+
+
+def dist((x1,y1), (x2,y2)):
+    return math.sqrt((x1-x2)**2+(y1-y2)**2)
+    
+    
+class PathView:
+    def __init__(self, name, path):
+        self.name = name
+        self.a = Point(*path.a)
+        self.b = Point(*path.b)+self.a
+        self.ac = Point(*path.ac)+self.a
+        self.bc = Point(*path.bc)+self.a
+        
+    def render(self):
+        c = Circle(
+            self.a[0],self.a[1],
+            width=10,color=(0.,.9,0.,1.)
+            )
+        c.render()
+        c = Circle(
+            self.ac[0],self.ac[1],
+            width=10,color=(0.,.9,0.5,1.)
+            )
+        c.render()
+        l = Line(self.a,self.ac,stroke=2,color=(0,1.,1.,1.))
+        l.render()
+        
+        c = Circle(
+            self.b[0],self.b[1],
+            width=10,color=(0.,.9,1.,1.)
+            )
+        c.render()
+        c = Circle(
+            self.bc[0],self.bc[1],
+            width=10,color=(0.,.9,1.,0.5)
+            )
+        c.render()
+        l = Line(self.b,self.bc,stroke=2,color=(0,1.,1.,1.))
+        l.render()
+        
+        steps=100.0
+        bz = self.bezier()
+        last = self.a
+        for i in range(steps):
+            t = (i+1)/steps
+            next = bz.at( t )
+            next = next[0] + self.a.x, next[1] + self.a.y
+            
+            glBegin(GL_LINES);
+            glVertex2f(last[0], last[1]); 
+            glVertex2f(next[0], next[1]); 
+            glEnd( );
+
+            last = next
+            
+    def get_nearest(self, x, y):
+        mind = 10000
+        near = self.a
+        for p in [self.a, self.b, self.ac, self.bc]:
+            if dist(p, (x,y)) < mind:
+                mind = dist(p, (x,y))
+                near = p
+                
+        return near
+        
+    def bezier(self):
+        return Bezier( 
+                (self.a.x, self.a.y),
+                (self.b.x-self.a.x, self.b.y-self.a.y),
+                (self.ac.x-self.a.x, self.ac.y-self.a.y),
+                (self.bc.x-self.a.x, self.bc.y-self.a.y),
+
+            )
+            
+    def repr(self):
+        return self.bezier().__repr__()
+        
+class Point:
+    def __init__(self, x, y):
+        self.set(x, y)
+        
+    def set(self, x, y):
+        self.x = x
+        self.y = y
+        
+    def __getitem__(self, item):
+        if item == 0: return self.x
+        elif item == 1: return self.y
+        raise IndexError("No such attr")
+
+    def __setitem__(self, item, value):
+        if item == 0: self.x = value
+        elif item == 1: self.y = value
+        raise IndexError("No such attr")
+    
+    def __add__(self, other):
+        return Point(self.x+other.x, self.y+other.y)
+    
+    
+class PathDraw(Layer):
+    SWITCH, SHOW, CREATE_A, CREATE_B, DRAG, ANIMATE = range(6)
+    
+    @property
+    def path(self):
+        return self.paths[self.pathp]
+
+    def __init__(self, module, filepath):
+        Layer.__init__(self)
+        self.module = module
+        self.filepath = filepath
+        self.paths = []
+        self.pathp = 0
+        self.near = None
+        for name in dir(module):
+            what = getattr(module, name)
+            if isinstance(what, Bezier):
+                p = PathView( name, what )
+                self.paths.append( p )
+        if len(self.paths)==0:
+            self.new_path()
+        self.state = self.SHOW
+        self.stop = False
+        self.mouse = Point(0,0)
+
+        self.font = font.load('Arial', 18)
+        self.duration = 3
+        self.time_warp = 1
+        self.number = ""
+        
+    def save(self):
+        text = "from cocos.path import Bezier\n\n"
+        for p in self.paths:
+            text += p.name + " = "
+            text += p.bezier().__repr__()
+            text += "\n"
+            
+        
+        f = open(self.filepath, "w")
+        f.write( text )
+        f.close()
+        
+    def on_text(self, text):
+        if self.state in ( self.SHOW, self.SWITCH ):
+            if text in string.digits:
+                self.state = self.SWITCH
+                self.number += text
+        
+        
+    def on_mouse_motion(self, x, y, dx, dy):
+        self.mouse.set(x,y)
+        if self.state == self.SHOW:
+            near = self.path.get_nearest(x,y)
+            if dist(near, (x,y)) < 8:
+                self.near = near
+            else:
+                self.near = None
+        if self.state == self.DRAG:
+            self.near.set(x,y)       
+                
+    def on_key_press(self, symbol, modifiers):
+        if self.state == self.SHOW:
+            if symbol == key.S:
+                self.save()
+            if symbol == key.N:
+                self.new_path()
+            if symbol == key.A:
+                self.state = self.ANIMATE
+                self.anim_start = time.time()
+            if symbol == key.D:
+                if (modifiers & key.LSHIFT or modifiers & key.RSHIFT):
+                    self.duration /= 1.1
+                else:
+                    self.duration *= 1.1
+                
+            if symbol == key.T:
+                if (modifiers & key.LSHIFT or modifiers & key.RSHIFT):
+                    self.time_warp /= 1.1
+                else:
+                    self.time_warp *= 1.1
+        elif self.state == self.SWITCH:
+            if symbol == key.ENTER:
+                try:
+                    newp = int(self.number)
+                    if newp < len(self.paths):
+                        self.pathp = newp
+                except ValueError:
+                    pass
+                self.state = self.SHOW
+                self.number = ""
+                       
+                    
+    def on_mouse_press(self, x, y, button, modifiers):
+        self.mouse.x = x
+        self.mouse.y = y
+        
+        if self.state == self.SHOW:
+            if button == mouse.LEFT:
+                if self.near:
+                    self.state = self.DRAG
+                    
+        elif self.state == self.DRAG:
+            if button == mouse.LEFT:
+                self.state = self.SHOW       
+        
+    def draw(self):
+        if self.stop:
+            director.scene.end()
+            
+        if self.state == self.SWITCH:
+            text = font.Text(self.font, 'switch to:',color=(0.,0.5,0.5,0.5))
+            text.x = 20
+            text.y = 300
+            text.draw()
+            text = font.Text(self.font, self.number ,color=(0.,0.5,0.5,0.5))
+            text.x = 20
+            text.y = 250
+            text.draw()
+            
+        elif self.state in (self.SHOW, self.DRAG):
+            self.path.render()
+            if self.near:
+                c = Circle(
+                    self.near.x,self.near.y,
+                    width=15,color=(0.,1.,1.,1.)
+                    )
+                c.render()
+                
+        elif self.state == self.ANIMATE:
+            dt = time.time() - self.anim_start
+            bz = self.path.bezier()
+            if dt >= self.duration:
+                self.state = self.SHOW
+            else:
+                x,y = bz.at( (dt/self.duration)**self.time_warp )
+                c = Circle(
+                        x+self.path.a.x,y+self.path.a.y,
+                        width=45,color=(0.,1.,1.,1.)
+                        )
+                c.render()
+                    
+                
+        text = font.Text(self.font, '[%i,%i] t**%0.2f d=%0.2f n=%d p=%d'%(
+                    self.mouse.x, self.mouse.y,
+                    self.time_warp,
+                    self.duration,
+                    len(self.paths), self.pathp
+                ),
+                color=(0.,0.5,0.5,0.5))
+        text.x = 100
+        text.y = 20
+        text.draw()
+
+                 
+    def new_path(self):
+        self.paths.append( PathView("new"+str(random.randint(1,1000000)), Bezier( (100,100), (200,200), (108, 268), (438,27) ) ) )
+        self.pathp = len(self.paths)-1
+        
+ 
+
+            
+        
+        
+if __name__ == "__main__":
+    import imp, sys
+    path = sys.argv[1]
+    client = imp.load_source("client", path)
+    director.init()
+    draw = PathDraw(client, path)
+    director.run( Scene( draw ) )
---
+++ tools/primitives.py
@@ -0,0 +1,447 @@
+#!/usr/bin/env python
+"""
+Implementing 2D drawing primitives using
+pyglet.gl
+copyright 2007 by Flavio Codeco Coelho
+"""
+from pyglet import font
+from pyglet import clock
+from pyglet import window
+from pyglet import image
+from pyglet.gl import *
+from pyglet.window import mouse
+from pyglet.window import event
+from pyglet.window import key
+
+class Base(object):
+    """
+    Basic attributes of all drawing primitives
+    """
+    def __init__(self, x, y, z=0, color=(0.,0.,0.,1.), stroke=0, rotation=0):
+        try :
+            self.rect
+        except AttributeError:
+            self.rect = Rect(x,y,1,1) # this inits x,y and loc as well
+        self.visible = 1 #
+        self.z = z 
+        self.rotation = rotation
+        self.stroke = stroke
+        self.color = color
+        self.q = gluNewQuadric()
+    def setLoc(self, p) : self.rect.loc = p 
+    def getLoc(self) : return self.rect.loc
+    def setX(self, x) : self.rect.x = x 
+    def getX(self) : return self.rect.x 
+    def setY(self, y) : self.rect.y = y 
+    def getY(self) : return self.rect.y
+    loc = property(getLoc, setLoc)
+    x = property(getX, setX)
+    y = property(getY, setY)
+    def setWidth(self,w) : self.rect.width = w
+    def getWidth(self) : return self.rect.width
+    def setHeight(self, h) : self.rect.height = h
+    def getHeight(self) : return self.rect.height
+    width = property(getWidth, setWidth)
+    height = property(getHeight, setHeight)
+        
+class Pixel(Base):
+    """ A pixel at a given x,y,z position and color.
+        Pixel(x=12, y=100, z=900, color=(1,0,0,0.5))
+    """
+    def render(self):
+        """
+            Draws a pixel at a given x and y with given color .
+            Color = 3 or 4 arg tuple. RGB values from 0 to 1 being 1 max value (1, 1, 1) would be white
+        """
+        glColor4f(*self.color)
+##        glDisable(GL_TEXTURE_2D) # disable in case it was on
+            
+        glPushMatrix() # remember previous matrix state before translating, rotating
+        glTranslatef(self.x, self.y, -self.z) # translate to point to draw
+
+        glBegin(GL_POINTS) # draw point
+        glVertex3f(0.0, 0.0, 0.0)
+        glEnd()
+
+        glPopMatrix() # back to previous matrix state
+
+    def intersects(self, x,y):
+        if x==self.x and y==self.y : return True
+
+class Circle(Base):
+    """ Circle class
+        Circle(x=20, y=100, z=1, width=300, color=(1,1,0,0.3), stroke=5, rotation=0, style=GLU_FILL)
+        style choices are : GLU_LINE, GLU_FILL, GLU_SILHOUETTE, GLU_POINT
+    """
+    def __init__(self, x=10, y=10, z=0, width=2, color=(0,0,0,1), stroke=0, rotation=0.0, style=GLU_FILL):
+        self.radius = width*0.5
+        self.rect = Rect(x, y, width, width)
+        self.style = style
+        self.circleresolution = 60
+        Base.__init__(self, x,y,z,color, stroke, rotation)
+        
+    def setWidth(self, w):
+        self.radius = w*0.5
+        self.rect.width = w
+    width = property(Base.getWidth, setWidth)
+        
+    def render(self):
+        """ Draw Circle
+            x, y, z, width in pixel, rotation, color and line width in px
+            style choices are : GLU_LINE, GLU_FILL, GLU_SILHOUETTE, GLU_POINT
+            TO DO : textured circles
+        """
+        
+        glColor4f(*self.color)
+        glPushMatrix()
+
+        glTranslatef(self.x, self.y, -self.z)
+        glRotatef(self.rotation, 0, 0, 0.1)
+
+
+        if self.radius < 1 : self.radius = 1
+
+        if self.stroke :
+            inner = self.radius - self.stroke # outline width
+            if inner < 0: inner=0
+        else :
+             inner = 0 # filled
+        
+        gluQuadricDrawStyle(self.q, self.style)
+
+        gluDisk(self.q, inner, self.radius, self.circleresolution, 1) # gluDisk(quad, inner, outer, slices, loops)
+            
+        glPopMatrix()
+
+
+
+class Arc(Base):
+    """ Arc class
+        Arc(x=10, y=10, z=0, radius=1, start=0, sweep=1, color=(0,0,0,1), stroke=0, rotation=0.0, style=GLU_FILL)
+        style choices are : GLU_LINE, GLU_FILL, GLU_SILHOUETTE, GLU_POINT
+    """
+    def __init__(self, x=10, y=10, z=0, radius=1, start=0, sweep=1, color=(0,0,0,1), stroke=0,
+                 rotation=0.0, style=GLU_FILL):
+
+        Base.__init__(self, x,y,z,color, stroke, rotation)
+        self.radius = radius
+        self.start = start
+        self.sweep = sweep
+        self.style = style
+        self.circleresolution = 60
+        
+    def render(self):
+        """
+        Draws Arc
+        """
+        glColor4f(*self.color)
+        glPushMatrix()
+
+        glTranslatef(self.x, self.y, -self.z)
+        glRotatef(self.rotation, 0, 0, 0.1)
+
+        if self.stroke : 
+            inner = self.radius - self.stroke
+            if inner < 0: inner=0
+        else :
+            inner = 0 # full, no inner
+        self.start -= 180
+        
+        gluQuadricDrawStyle(self.q, self.style)
+        
+        gluPartialDisk(self.q, inner, self.radius, self.circleresolution, 1, self.start, self.sweep)
+        
+        glPopMatrix()
+
+
+
+class Polygon(Base):
+    def __init__(self, v, z=0, color=(0,0,0,1), stroke=0, rotation=0.0, style=0):
+        """ polygon class
+            Polygon(vertexarray=[(0, 0), (29, 100), (30, 200)], z=100, color=(0,0.3,0.1,1), stroke=0, rotation=23)
+            overwrites few methods from superclass as polygons are more complex, needs to update everyvertex.
+        """
+        self.v = v
+        l, t, r, b = calcPolygonRect(v) # get the bounding rect
+        self.rect = Rect(l+(r-l)*0.5, t+(b-t)*0.5, r-l, b-t)
+        self.v2 = [(i[0] - self.rect.x, i[1] - self.rect.y) for i in v] #relative polygon
+
+        self.style = style
+        
+        Base.__init__(self, self.rect.x, self.rect.y, z,color,stroke,rotation)
+
+    def updateV(self):
+        self.v = [(self.rect.x + n[0], self.rect.y + n[1]) for n in self.v2]
+
+    def setLoc(self, p):
+        self.rect.loc = p ; self.updateV()
+    def setX(self, x):
+        self.rect.x = x ; self.updateV()
+    def setY(self, y):
+        self.rect.y = y; self.updateV()
+    x = property(Base.getX, setX)
+    y = property(Base.getY, setY)
+    loc = property(Base.getLoc, setLoc)
+    
+    def render(self):
+        """ Draw Polygon
+            v is an array with tuple points like [(x, y), (x2, y2), (x3, y3)]
+            min vertex number to draw a polygon is 3
+            stroke=0 to fil with color the inside of the shape or stroke=N just to draw N-px thick outline.
+            Note. It doesnt work with non convex polygons, need to implement tesselation yet
+        """
+        l,t,r,b = calcPolygonRect(self.v)
+        x,y = calcRectCenter(l,t,r,b)
+        self.drawVertex(x, y, self.z, [(i[0] - x, i[1] - y) for i in self.v], self.color, self.stroke, self.rotation, self.style)
+
+    
+    def drawVertex(self, x, y,  z=0, v=(), color=(0,0,0,1), stroke=0, rotation=0.0,   style=0):
+        
+        glColor4f(*self.color)    
+        glPushMatrix()
+
+        glTranslatef(x, y, -z)
+        glRotatef(self.rotation, 0, 0, 0.1)
+
+        if self.style :
+            glEnable(GL_LINE_STIPPLE)
+            glLineStipple(1, style)
+##        else :
+##            glDisable(GL_LINE_STIPPLE)
+##            0xF0F0 # dashed line
+##            0xF00F # long dashed line
+##            0x8888 # dotted lines
+##        glRect(x1,y,1,x1,x2)
+##        glRectiv(v1,v2) # oposite vertex of rectangle
+        # -- start drawing
+        if self.stroke : # outlined polygon
+            glLineWidth(self.stroke)
+            glBegin(GL_LINE_LOOP)
+        else: # filled polygon
+            if   len(v) == 4 : glBegin(GL_QUADS)
+            elif len(v)  > 4 : glBegin(GL_POLYGON)
+            else :             glBegin(GL_TRIANGLES) # which type of polygon are we drawing?
+
+        for p in v:
+            glVertex3f(p[0], p[1],0)  # draw each vertex
+
+        glEnd()
+        # -- end drawing
+        
+        if self.style : glDisable(GL_LINE_STIPPLE)
+        
+        glPopMatrix()
+
+
+class LineRel(Base):
+    def __init__(self, x,y, a=(0,0), b=(0,0), z=0, color=(0,0,0,1), stroke=0, rotation=0.0, style=0):
+        """ Draws a basic line given the begining and end point (tuples), color (tuple) and stroke
+            (thickness of line)
+            Line( x,y, a=(1,1), b=(100,100), z=0, color=(0.2,0,0,1), stroke=10, rotation=45)
+        """
+        w = (b[0] - a[0]) 
+        h = (b[1] - a[1]) 
+        x = abs(a[0] + w*0.5)
+        y = abs(a[1] + h*0.5)
+        self.a2 = abs(a[0]) - x, abs(a[1]) - y
+        self.b2 = abs(b[0]) - x, abs(b[1]) - y
+        self.a = x - w*0.5, y - w*0.5
+        self.b = x + w*0.5, y + w*0.5
+        self.rect = Rect(x, y, w, h)
+        self.style = style
+        Base.__init__(self, x, y, z,color,stroke,rotation)
+
+    def render(self):
+        """
+        Draws Line
+        """
+        p1 = self.a2
+        p2 = self.b2
+        glColor4f(*self.color)
+        color  = (GLfloat *4)(*self.color)
+               
+        glPushMatrix()
+
+        glTranslatef(self.x, self.y, -self.z) # translate to GL loc ppint
+        glRotatef(self.rotation, 0, 0, 0.1)
+
+        if self.style :
+            glEnable(GL_LINE_STIPPLE)
+            glLineStipple(1, self.style)
+##        else :
+##            glDisable(GL_LINE_STIPPLE)
+            
+        if self.stroke <= 0:
+            self.stroke = 1
+        glLineWidth(self.stroke)
+
+        glBegin(GL_LINES)
+        glVertex2i(int(p1[0]), int(p1[1])) # draw pixel points
+        glVertex2i(int(p2[0]), int(p2[1]))
+        glEnd()
+
+        if self.style :
+            glDisable(GL_LINE_STIPPLE)
+        
+        glPopMatrix()
+
+
+    def updateAB(self):
+        self.a = self.x + self.a[0], self.y + self.a[0]
+        self.b = self.x + self.b[0], self.y + self.b[0]
+
+    def setLoc(self, p):
+        self.rect.loc = p ; self.updateAB()
+    def setX(self, x):
+        self.rect.x = x ; self.updateAB()
+    def setY(self, y):
+        self.rect.y = y; self.updateAB()
+    x = property(Base.getX, setX)
+    y = property(Base.getY, setY)
+    loc = property(Base.getLoc, setLoc)
+
+
+class Line(LineRel):
+    def __init__(self, a=(0,0), b=(0,0), z=0, color=(0,0,0,1), stroke=0, rotation=0.0, style=0):
+        """ Draws a basic line given the begining and end point (tuples), color (tuple) and stroke
+            (thickness of line)
+            Line( a=(1,1), b=(100,100), z=20, color=(0.2,0,0,1), stroke=10, rotation=45)
+        """
+        w = (b[0] - a[0]) 
+        h = (b[1] - a[1]) 
+        x = abs(a[0] + w*0.5) # abs x,y
+        y = abs(a[1] + h*0.5)
+        a = x-w*0.5, y-h*0.5 # relative a,b
+        b = x+w*0.5, y+h*0.5
+        LineRel.__init__(self, x, y, a, b, z, color, stroke, rotation, style)
+
+        
+
+# intersectable shapes (draggable)
+
+class Rect(object):
+    def __init__(self, x=0,y=0,w=0,h=0):
+        """ rect(self, x=0,y=0,w=0,h=0)
+            x,y,loc, width, height
+            left,top,right,bottom
+            quad ->
+            --------------------
+            topleft = quad[0]
+            topright = quad[1]
+            bottomright = quad[2]
+            bottomleft = quad[3]
+        """
+        self.rect = x,y,w,h
+
+    def setRect(self, r):
+        self.__x = r[0]
+        self.__y = r[1]
+        self.__width = r[2]
+        self.__height = r[3]
+        w = r[2]*0.5 ; h = r[3]*0.5
+        self.__rect = r[0]-w, r[1]-h, r[0]+w, r[1]+h # l t r b
+    def getRect(self):
+        return self.__rect
+    rect = property(getRect, setRect)
+
+    def setQuad(self, q): # [ q[0][0], q[0][1], q[1][0], q[2][1] ] # l t r b
+        self.rect = q[0][0]+(q[1][0]-q[0][0])*0.5, q[0][1]+(q[2][1]-q[0][1])*0.5, q[1][0]-q[0][0], q[2][1]-q[0][1] 
+    def getQuad(self):
+        return [(self.rect[0], self.rect[1]),(self.rect[2], self.rect[1]),(self.rect[2], self.rect[3]),(self.rect[0], self.rect[3])] # tl tr br bl
+    quad = property(getQuad, setQuad)
+
+    def setX(self, x):
+        self.rect = x, self.y, self.width, self.height
+    def getX(self) : return self.__x
+    x = property(getX, setX)
+
+    def setY(self, y):
+        self.rect = self.x, y, self.width, self.height
+    def getY(self) : return self.__y
+    y = property(getY, setY)
+
+    def setLoc(self, p):
+        self.rect = p[0], p[1], self.width, self.height
+    def getLoc(self) : return self.__x, self.__y # self.x, self.y
+    loc = property(getLoc, setLoc)
+
+    def setWidth(self, w):
+        self.rect = self.x, self.y, w, self.height
+    def getWidth(self): return self.__width
+    width = property(getWidth, setWidth)
+    
+    def setHeight(self, h):
+        self.rect = self.x, self.y, self.width, h
+    def getHeight(self): return self.__height
+    height = property(getHeight, setHeight)
+
+    def setLeft(self,x):
+        self.rect = x+self.width*0.5, self.y, self.width, self.height
+    def getLeft(self): return self.rect[0]
+    left = property(getLeft, setLeft)
+    
+    def setTop(self,y):
+        self.rect = self.x, y+self.width*0.5, self.width, self.height
+    def getTop(self): return self.rect[1]
+    top = property(getTop, setTop)
+    
+    def setRight(self,x):
+        self.rect = x-self.width*0.5, self.y, self.width, self.height
+    def getRight(self): return self.rect[2]
+    right = property(getRight, setRight)
+    
+    def setBottom(self,x):
+        self.rect = self.x, y-self.width*0.5, self.width, self.height
+    def getBottom(self): return self.rect[3]
+    bottom = property(getBottom, setBottom)
+
+
+def calcPolygonRect(pointArray):
+    """ receives a point list and returns the rect that contains them as a tupple -> tuple left, top, right, bottom
+    """
+    # init to ridiculously big values. not very elegant or eficient
+    l, t, r, b = 10000000, 10000000, -10000000, -10000000
+##    l = pointArray[0]
+##    t = pointArray[1]
+##    r = l
+##    b = t
+
+    for n in pointArray: # calc bounding rectangle rect
+        if n[0] < l : l = n[0]
+        if n[0] > r : r = n[0]
+        if n[1] < t : t = n[1]
+        if n[1] > b : b = n[1]
+
+    return l, t, r, b
+
+
+def calcRectCenter(l,t,r,b):#,v=()):
+    """ returns rect center point -> x,y
+        calcRectCenter(l,t,r,b)
+    """
+##    if len(v) : l,t,r,b = v[0],v[1],v[2],v[3]
+    return l+((r-l)*0.5), t+((b-t)*0.5)
+
+if __name__=="__main__":
+    import random
+    win = window.Window()
+    glEnable(GL_BLEND)
+    
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+    p = Pixel(10,10)
+    c = Circle(200,100,width=100,color=(0.,.9,0.,1.))
+    a = Arc(150,150,radius=100,color=(1.,0.,0.,1.),sweep=90,style=GLU_FILL)
+    P = Polygon([(0, 0), (50, 200), (80, 200),(60,100),(100,5)],color=(.3,0.2,0.5,.7))
+    l = Line((10,299),(100,299),stroke=2,color=(0,0.,1.,1.))
+    while not win.has_exit:
+        win.dispatch_events()
+        col = [random.random() for i in xrange(3)]+[1]
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        c.color = col
+        c.render()
+        p.render()
+        a.render()
+        a.rotation+=1
+        P.render()
+        l.render()
+        win.flip()
