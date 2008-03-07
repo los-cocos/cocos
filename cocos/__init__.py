7963bfc58f77fd5127144f6c07c98858fa2cbf9f
Ricardo Quesada <ricardoquesada@gmail.com>
2008-03-07 18:02:13 +0000

    version is 0.2.0

--- cocos/__init__.py
+++ cocos/__init__.py
@@ -4,16 +4,13 @@ Los Cocos: An extension for Pyglet
 http://code.google.com/p/los-cocos/
 """
 
-__version__ = "0.1"
-__author__ = "PyAr"
+__version__ = "0.2.0"
+__author__ = "PyAr Team"
 
 import actions
 import director
 import effect
-import euclid
 import layer
 import menu
 import path
-import primitives
 import scene
-import particle
