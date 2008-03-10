d23e60ad2754f92d92e3ab8f8649450a782efa76
Ricardo Quesada <ricardoquesada@gmail.com>
2008-03-10 18:59:54 +0000

    added CHANGELOG file added warning if pyglet.version is not 1.0 improved README file

--- cocos/__init__.py
+++ cocos/__init__.py
@@ -6,6 +6,7 @@ http://code.google.com/p/los-cocos/
 
 __version__ = "0.1.1"
 __author__ = "PyAr Team"
+version = __version__
 
 import actions
 import director
@@ -16,3 +17,13 @@ import menu
 import path
 import scene
 import particle
+
+import pyglet
+if pyglet.version.find( '1.0' ) == -1:
+    for i in range(3):
+        print "*" * 80
+    print "cocos v%s does not work properly with pyglet v%s" % (__version__, pyglet.version )
+    print "\nUse pyglet v1.0 or upgrade to cocos-0.2.x\n"
+    for i in range(3):
+        print "*" * 80
+    
