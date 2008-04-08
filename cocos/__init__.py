468a3bd061a5382e993cbde27216ee939548cbc0
Ricardo Quesada <ricardoquesada@gmail.com>
2008-04-08 19:18:10 +0000

    0.3.0alpha0

--- cocos/__init__.py
+++ cocos/__init__.py
@@ -4,8 +4,8 @@ Los Cocos: An extension for Pyglet
 http://code.google.com/p/los-cocos/
 """
 
-__version__ = "0.3.0"
-__author__ = "PyAr Team"
+__version__ = "0.3.0-alpha0"
+__author__ = "Cocos Team"
 version = __version__
 
 import actions
