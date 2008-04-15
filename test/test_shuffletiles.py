6ee6fbba9e079a7be6e7838da47c0bc71fc7b63d
Ricardo Quesada <ricardoquesada@gmail.com>
2008-04-15 17:22:27 +0000

    unit test for shuffletest

---
+++ test/test_shuffletiles.py
@@ -0,0 +1,32 @@
+# This code is so you can run the samples without installing the package
+import sys
+import os
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
+#
+
+
+import cocos
+from cocos.director import director
+from cocos.actions import *
+from cocos.layer import *
+from cocos.sprite import ActionSprite
+import pyglet
+import random
+
+from cocos.mesh import *
+
+
+if __name__ == "__main__":
+    director.init( resizable=True, fullscreen=False )
+    director.show_FPS = True
+    main_scene = cocos.scene.Scene()
+
+    for i in range(16):
+        l = ColorLayer(random.random(), random.random(), random.random(), 1.0)
+        scale = (16-i)/16.0
+        main_scene.add( l, z=i, scale=scale )
+
+    e = ShuffleTiles( x_quads=8, y_quads=8, duration=5 )
+    main_scene.do( e )
+
+    director.run (main_scene)
