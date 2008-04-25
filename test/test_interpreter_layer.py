3d35a84d92123fcf530cb366d60de0f2c45d1c18
Ricardo Quesada <ricardoquesada@gmail.com>
2008-04-25 21:27:41 +0000

    test python interpreter

---
+++ test/test_interpreter_layer.py
@@ -0,0 +1,17 @@
+# This code is so you can run the samples without installing the package
+import sys
+import os
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
+#
+
+
+import cocos
+from cocos.director import director
+import pyglet
+        
+
+if __name__ == "__main__":
+    director.init()
+    interpreter_layer = cocos.layer.InterpreterLayer()
+    main_scene = cocos.scene.Scene(interpreter_layer)
+    director.run(main_scene)
