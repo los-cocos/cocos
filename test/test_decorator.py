24c83fe851bee8b65d6ccc666659a859545add8d
Lucio Torre <lucio.torre@gmail.com>
2008-05-08 23:55:58 +0000

    added an example of using CallFuncS as a decorator.

---
+++ test/test_decorator.py
@@ -0,0 +1,34 @@
+# This code is so you can run the samples without installing the package
+import sys
+import os
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
+#
+
+
+import cocos
+from cocos.director import director
+from cocos.actions import CallFuncS, Show
+from cocos.sprite import ActionSprite
+import pyglet
+
+class TestLayer(cocos.layer.Layer):
+    def __init__(self):
+        super( TestLayer, self ).__init__()
+        
+        x,y = director.get_window_size()
+        
+        self.sprite = ActionSprite('grossini.png', (x/2, y/2) )
+        self.sprite.visible = False
+        self.add( self.sprite )
+        
+        @CallFuncS
+        def make_visible( sp ):
+            sp.do( Show() )
+        self.sprite.do( make_visible )
+        
+
+if __name__ == "__main__":
+    director.init()
+    test_layer = TestLayer ()
+    main_scene = cocos.scene.Scene (test_layer)
+    director.run (main_scene)
