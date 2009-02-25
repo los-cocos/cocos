14a1ba49aa10a1e7311b944ac6713558a5747bfe
Lucio Torre <lucio.torre@gmail.com>
2009-02-25 02:11:52 +0000

    added test draw line

---
+++ test/test_draw_line.py
@@ -0,0 +1,27 @@
+# This code is so you can run the samples without installing the package
+import sys
+import os
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
+#
+
+
+import cocos
+from cocos.director import director
+from cocos.sprite import Sprite
+from cocos.actions import Lerp, Reverse, Repeat
+from cocos import draw
+import pyglet, math
+    
+        
+class TestLayer(cocos.layer.Layer):
+    def __init__(self):
+        super( TestLayer, self ).__init__()
+        line = draw.Line((0,0), (100,100), (255,255,255,255))
+        self.add( line )        
+
+if __name__ == "__main__":
+    director.init()
+    test_layer = TestLayer ()
+    main_scene = cocos.scene.Scene (test_layer)
+    director.run (main_scene)
+
