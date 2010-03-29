a6605b75dcae162389689c5249f8fa4a6679e4e0
Richard Jones <r1chardj0n3s@gmail.com>
2010-03-29 21:20:20 +0000

    another place for the version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ $Date: 2008-04-05 16:22:08 -0500 (Sat, 05 Apr 2008) $
 
 __author__ = "cocos2d team"
 __author_email__ = "lucio.torre@gmail.com"
-__version__ = "0.3.0"
+__version__ = "0.4.0b1"
 __date__ = "2008-06-27"
 
 try:
