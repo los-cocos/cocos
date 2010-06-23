0dcb5e5a968f4ba7c6cdb6b1e1919592fe8b4b4b
Claudio Canepa <ccanepacc@gmail.com>
2010-06-23 22:15:34 +0000

    unit test for IntervalAction + Action
    -
    when the IntervalAction has duration = 0.0 his update(1.0) can be missed.
    Thanks Ivo F.A.C. Fokkema <ivo.fokkema@gmail.com> for the initial report,
    see thread
    http://thread.gmane.org/gmane.comp.python.cocos2d.user/1093

---
+++ utest/test_p_ba_operands_different_classes.py
@@ -0,0 +1,96 @@
+# important: set cocos_utest=1 in the environment before run.
+# that simplifies the pyglet mockup needed
+# remember to erase or set to zero for normal runs
+import os
+assert os.environ['cocos_utest']
+
+# set the desired pyglet mockup
+import sys
+sys.path.insert(0,'pyglet_mockup1')
+import pyglet
+assert pyglet.mock_level == 1 
+
+import cocos
+from cocos.director import director
+from cocos.cocosnode import CocosNode
+import cocos.actions as ac
+
+import sys
+
+director.init()
+
+rec = []
+next_done=0 #bitflags
+class UAction(ac.Action):
+##use actions names 1, 2 or '1', '2' ; then you instruct the .step method
+##to set ._done using the global next_done
+    def init(self, name):
+        rec.append((name, 'init'))
+        self.name = name
+
+    def start(self):
+        rec.append((self.name, 'start'))
+
+    def step(self, dt):
+        global rec, next_done
+        rec.append((self.name, 'step', dt))
+        if int(self.name) & next_done:
+            print 'setting %s _done to True'%self.name
+            self._done = True
+
+    def stop(self):
+        rec.append((self.name, 'stop'))
+
+##    def done(self):
+##        rec.append((self.name, 'done', 
+
+class UIntervalAction(ac.IntervalAction):
+    def init(self, name, duration):
+        rec.append((name, 'init'))
+        self.duration = duration
+        self.name = name
+
+    def start(self):
+        rec.append((self.name, 'start'))
+
+    def step(self, dt):
+        rec.append((self.name, 'step', dt))
+        super(UIntervalAction, self).step(dt)
+
+    def update(self, fraction):
+        rec.append((self.name, 'update', fraction))
+
+    def stop(self):
+        rec.append((self.name, 'stop'))
+
+##    def done(self):
+##        rec.append((self.name, 'done', 
+
+
+class Test_sequence:
+    """
+    """
+    def test_InstantAction_duration0_action(self):
+        global rec
+        node = CocosNode()
+        name1 = '1'
+        name2 = '2'
+        duration1 = 0.0
+        a = UIntervalAction(name1, duration1)
+        b = UAction(name2)
+        composite = a + b
+        rec = []
+        worker = node.do(composite)
+        dt = 0.1
+        node._step(dt)
+        print rec
+        assert rec[0] == (name1, 'start')
+        assert rec[1] == (name1, 'step', dt)
+        assert rec[2] == (name1, 'update', 1.0)
+        assert rec[3] == (name1, 'stop')
+        assert rec[4] == (name2, 'start')
+
+        rec = []
+        node._step(dt)
+        for e in rec:
+            assert e[0]==name2
