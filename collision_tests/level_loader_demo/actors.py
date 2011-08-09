5413d616d31368da1a0c035ca2d0029e43c21337
Claudio Canepa <ccanepacc@gmail.com>
2011-08-09 17:14:02 +0000

    protoeditor: make actual runner the level_loader_demo

---
+++ collision_tests/level_loader_demo/actors.py
@@ -0,0 +1,82 @@
+import operator
+import pprint
+
+import cocos
+import cocos.euclid as eu
+import cocos.collision_model as cm
+
+class BaseActor(cocos.sprite.Sprite):
+    # loadable subclasses should set
+    # ingame_type_id = '...'
+
+    @classmethod
+    def new_from_dict(cls, game, args, description_dict):
+        """
+        don't modify, autority is editor
+
+        args provides aditional positional arguments that must reach the
+        __init__. These arguments don't come from the stored map, they are
+        generated at game runtime just before calling here.
+        Look at the manual for extra info
+
+        description_dict carries the info for this instantiation comming
+        from the stored level  
+        """
+        desc = description_dict
+        ingame_type_id = description_dict.pop('ingame_type_id')
+        editor_type_id = description_dict.pop('editor_type_id')
+        combo_type = (editor_type_id, ingame_type_id)
+        editor_img = game['roles']['actor'][combo_type]['editor_img']
+        # it is a pyglet resource, so use '/' always to concatenate
+        ingame_img = 'data/images/' + editor_img
+        cx = description_dict.pop('cx')
+        cy = description_dict.pop('cy')
+        all_args = [ingame_img, cx, cy]
+        all_args.extend(args) 
+        actor = cls(*all_args, **description_dict)
+        return actor
+
+    def __init__(self, default_img, cx, cy,
+                 visible_width=32, others={}):
+        """
+        Adding or removing parameters must follow special rules to not break
+        load-store, look in the manual in secctions named 'Changing code'
+        """
+        # cx, cy, visible_width in world units
+        super(BaseActor, self).__init__(default_img)
+        center = eu.Vector2(cx, cy)
+        self.cshape = cm.AARectShape(center, 1.0, 1.0)
+        self.update_visible_width(visible_width)
+        self.update_center(center)
+        # process 'others' if necesary
+        #...
+
+    def update_center(self, new_center):
+        assert isinstance(new_center, eu.Vector2) 
+        self.position = new_center
+        self.cshape.center = new_center
+
+    def update_visible_width(self, visible_width):
+        self.visible_width = visible_width
+        self.scale =  float(visible_width) / self.image.width
+        rx = visible_width / 2.0
+        ry = self.image.height / 2.0 * self.scale
+        self.cshape.rx = rx
+        self.cshape.ry = ry
+
+
+class Player(BaseActor):
+    ingame_type_id = 'player 00.01'
+
+class EnemyWanderer(BaseActor):
+    ingame_type_id = 'wanderer 00.01'
+
+class EnemyChained(BaseActor):
+    ingame_type_id = 'chained mons 00.01'
+
+class Tree(BaseActor):
+    ingame_type_id = 'tree 00.01'
+
+class Jewel(BaseActor):
+    ingame_type_id = 'jewel 00.01'
+    
---
+++ collision_tests/level_loader_demo/runner.py
@@ -0,0 +1,65 @@
+"""
+demo for load and intantiation of levels created with protoeditor
+
+It doens't care to draw a background for simplicity.
+Scale is different from the one seen in editor to demostrate how
+you define how much of world can be seen on screen; to match the editor
+view set the same width, height in both consts['window'] and consts['view']
+"""
+
+import sys
+import os
+import pprint
+
+import pyglet
+from pyglet.window import key
+from pyglet.window import mouse
+from pyglet.gl import *
+
+import cocos
+from cocos.director import director
+import loader as lo
+
+from gamedef import game
+
+consts = {
+    "window": {
+        # make sure width/height here is the same as width/height in view
+        "width": 640,
+        "height": 480,
+        "vsync": True,
+        "resizable": False
+        },
+    "view": {
+        # how much of world can be seen at every time, in world units
+        "width": 400.0,
+        "height": 300.0
+        }
+    }
+
+print __doc__
+
+# ensure we have a context
+director.init(**consts["window"])
+
+# get access to game resources
+# tip: don't change the location of starting script relative to gamedir along
+# the development
+pyglet.resource.reindex()
+# uncomment to see which resources can see pyglet
+##print '\nresources:'
+##pprint.pprint(pyglet.resource._default_loader._index)
+
+# build the scene
+scene = cocos.scene.Scene()
+scroller = cocos.layer.ScrollingManager()
+scene.add(scroller)
+
+worldview = lo.load_level('data/levels/level_00.lvl')
+zoom = 1.0
+scroller.scale = zoom * consts['window']['width'] / float(consts['view']['width'])
+
+scroller.add(worldview)
+
+# run the scene
+director.run(scene)
