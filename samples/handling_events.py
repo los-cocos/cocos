2289c3df7c38c2143a451e440277a8b711d2b493
Claudio Canepa <ccanepacc@gmail.com>
2012-08-12 03:28:14 +0000

    clarifing mouse drag for programming guide, issue 183

--- samples/handling_events.py
+++ samples/handling_events.py
@@ -76,13 +76,26 @@ class MouseDisplay(cocos.layer.Layer):
 
 
     def on_mouse_motion (self, x, y, dx, dy):
-        """This function is called when the mouse is moved over the app.
+        """Called when the mouse moves over the app window with no button pressed
         
         (x, y) are the physical coordinates of the mouse
         (dx, dy) is the distance vector covered by the mouse pointer since the
           last call.
         """
         self.update_text (x, y)
+
+    def on_mouse_drag (self, x, y, dx, dy, buttons, modifiers):
+        """Called when the mouse moves over the app window with some button(s) pressed
+        
+        (x, y) are the physical coordinates of the mouse
+        (dx, dy) is the distance vector covered by the mouse pointer since the
+          last call.
+        'buttons' is a bitwise or of pyglet.window.mouse constants LEFT, MIDDLE, RIGHT
+        'modifiers' is a bitwise or of pyglet.window.key modifier constants
+           (values like 'SHIFT', 'OPTION', 'ALT')
+        """
+        self.update_text (x, y)
+
 
     def on_mouse_press (self, x, y, buttons, modifiers):
         """This function is called when any mouse button is pressed
