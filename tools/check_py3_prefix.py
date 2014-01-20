2a66de8fc44e6c4e34b3cdef608f72880128a0c4
Claudio Canepa <ccanepacc@gmail.com>
2014-01-20 19:20:12 +0000

    py3: updated helper check_py3_prefix.py

--- tools/check_py3_prefix.py
+++ tools/check_py3_prefix.py
@@ -2,17 +2,18 @@ from __future__ import division, print_function, unicode_literals
 
 import os
 
-#base = r"C:\cocos_pristine\cocos_trunk_clean\test"
-base = "../test"
+dirs = ["../test", "../utest"]
 must_have = "from __future__ import division, print_function, unicode_literals"
 non_compliant = []
-for shortname in os.listdir(base):
-    fname = os.path.join(base, shortname)
-    if fname.endswith('.py'):
-        with open(fname, "r") as f:
-            text = f.read()
-            if must_have not in text:
-                non_compliant.append(fname)
+
+for base in dirs:
+    for shortname in os.listdir(base):
+        fname = os.path.join(base, shortname)
+        if fname.endswith('.py'):
+            with open(fname, "r") as f:
+                text = f.read()
+                if must_have not in text:
+                    non_compliant.append(fname)
 
 if non_compliant:
     print("Files which don't have the stock py3 compat line:\n")
