cd03ac2e917d0a82f8abdcc96e0a720f503167f2
Claudio Canepa <ccanepacc@gmail.com>
2014-03-13 19:43:00 +0000

    b_py3: fix euclid 'if vector:' in py3

--- cocos/euclid.py
+++ cocos/euclid.py
@@ -22,16 +22,16 @@
 '''euclid graphics maths module
 
 Documentation and tests are included in the file "euclid.txt", or online
-at https://github.com/euclid3/euclid3
-
-revision of euclid3/euclid3 euclid included: 2014 02 20 sha: de28ea080370 
+at http://code.google.com/p/pyeuclid
 '''
 
 from __future__ import division, print_function, unicode_literals
 import sys
 if sys.version_info[0] > 2:
+    PY2 = False
     scalar_types = (int, float)
 else:
+    PY2 = True
     scalar_types = (int, long, float) 
 
 __docformat__ = 'restructuredtext'
@@ -84,8 +84,12 @@ class Vector2(Slotted):
     def __ne__(self, other):
         return not self.__eq__(other)
 
-    def __nonzero__(self):
-        return self.x != 0 or self.y != 0
+    if PY2:
+        def __nonzero__(self):
+            return self.x != 0 or self.y != 0
+    else:
+        def __bool__(self):
+            return self.x != 0 or self.y != 0
 
     def __len__(self):
         return 2
@@ -293,8 +297,12 @@ class Vector3(Slotted):
     def __ne__(self, other):
         return not self.__eq__(other)
 
-    def __nonzero__(self):
-        return self.x != 0 or self.y != 0 or self.z != 0
+    if PY2:
+        def __nonzero__(self):
+            return self.x != 0 or self.y != 0 or self.z != 0
+    else:
+        def __bool__(self):
+            return self.x != 0 or self.y != 0 or self.z != 0
 
     def __len__(self):
         return 3
