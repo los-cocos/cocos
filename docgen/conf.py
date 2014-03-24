a22c92002200964fe999319dab996d3db765b8b7
Claudio Canepa <ccanepacc@gmail.com>
2014-03-24 19:14:09 +0000

    update doc's conf.py

--- docgen/conf.py
+++ docgen/conf.py
@@ -74,7 +74,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'cocos2d'
-copyright = u'2013, cocos\'s team'
+copyright = u'2008-2014, cocos\'s team'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
