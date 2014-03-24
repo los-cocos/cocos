1994884dfed1f0e04abf29e974efc87f096c2ffb
Claudio Canepa <ccanepacc@gmail.com>
2014-03-24 15:32:35 +0000

    updating setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,28 @@
 # -*- coding: utf-8 -*-
 """setup -- setuptools setup file for cocos2d.
 
+Supports the usual 'setup.py install' for cocos.
+
+As a release builder:
+
    last used: release 0.6.0, built from py2.6 + setuptools 3.3
 
-   operation:
+   example operation for generating release without handling the docs:
        svn checkout http://los-cocos.googlecode.com/svn/trunk/ cocos_trunk
        cd cocos_trunk
        svn export . ../cocos_export
        cd ..\cocos_export
        py -2.6 setup.py sdist >../sdist.log
        [ the generated package will be in cocos_export/dist ]
+
+       Look at tools/building_release_notes.txt for more info about building
+       release.
 """
 
 __author__ = "cocos2d team"
 __author_email__ = "lucio.torre@gmail.com"
 __version__ = "0.6.0"
-__date__ = "2012 08 12"
+__date__ = "2014 03 24"
 
 try:
     import setuptools
@@ -37,7 +44,6 @@ setup(
     description = "a 2D framework for games and multimedia",
     long_description=long_description,
     url = "http://cocos2d.org",
-    download_url = "http://code.google.com/p/los-cocos/downloads/list",
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Environment :: MacOS X",
