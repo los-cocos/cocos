b0c9a28a1fd911973e43e6936f369f328fad5abc
Claudio Canepa <ccanepacc@gmail.com>
2015-04-08 00:01:23 -0300

    update build toolchain

--- tools/copy_docs.py
+++ tools/copy_docs.py
@@ -1,11 +1,14 @@
 """
 helper for the final release build of cocos
 
-It is meant to be run from a working copy or export of trunk, speifically
+It is meant to be run from a clean clone of cocos repo, specifically
 from one of the subdirs tools/ or docgen/
 
-It will copy generated documentation to the harcoded destination ..\cocos_build
-where the setuptools build is supossed to be generated.
+It will copy the sphinx generated documentation from the sphinx dest to
+($clone_topdir)\docs\html
+
+After the copy do a docgen/make clean (docgen/Makefile clean) to clean-up
+before packaging with setuptools
 
 See workflow in tools\building_release_notes.txt
 """
@@ -14,20 +17,21 @@ from __future__ import division, print_function, unicode_literals
 import os
 import shutil
 
-build_dir = "../../cocos_build"
-if not os.path.isdir(build_dir):
-    print("Error, directory %s must exist")
-    sys.exit(1)
-dst = os.path.join(build_dir, "doc/html")
-
 cwd = os.getcwd()
 if os.path.basename(cwd) == 'tools':
     src = "../docgen/_build/html"
 elif os.path.basename(cwd) == 'docgen':
     src = "_build/html"
 else:
-    print("This command must be run from the tools/ or docgen/ directory")
+    print("This command must be run from the tools/ or docgen/ directory.")
     sys.exit(1)
+
+if not os.path.isdir(src):
+    print("Error, directory %s must exist, probably need to build docs.")
+    sys.exit(1)
+
+top_dir = "../"
+dst = os.path.join(top_dir, "docs/html")
 
 if os.path.exists(dst):
     shutil.rmtree(dst)
