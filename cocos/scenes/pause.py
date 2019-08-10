60d24b7148b062316a08d03ab3a3f2fc2d6320f0
Richard Jones <r1chardj0n3s@gmail.com>
2019-08-10 11:25:19 +1000

    Migrate to pyglet 1.4

--- cocos/scenes/pause.py
+++ cocos/scenes/pause.py
@@ -90,12 +90,8 @@ class PauseLayer(Layer):
 
         x, y = director.get_window_size()
 
-        ft = pyglet.font.load('Arial', 36)
-        self.text = pyglet.font.Text(ft,
-                                     'PAUSED',
-                                     halign=pyglet.font.Text.CENTER)
-        self.text.x = x // 2
-        self.text.y = y // 2
+        self.text = pyglet.text.Label('PAUSED', font_name='Arial', font_size=36,
+                                      halign='center', x = x // 2, y = y // 2)
 
     def draw(self):
         self.text.draw()
